ee6c9caf50a2c894c326e1a3d2167da4c0100b98
Stompf <anton.lundmark@gmail.com>
2022-12-07 12:59:23 +0100

    day6

---
+++ 2022/day6/2.ts
@@ -0,0 +1,30 @@
+import path from "path";
+import fs from "fs";
+
+function main(path: string) {
+    const content = fs.readFileSync(path, "utf-8").split("");
+
+    let curr = [] as string[];
+
+    for (let index = 0; index < content.length; index++) {
+        const element = content[index];
+        curr.push(element);
+
+        if (curr.length < 14) {
+            continue;
+        }
+
+        if (curr.length > 14) {
+            curr.shift();
+        }
+
+        const set = new Set(curr);
+        if (set.size === curr.length) {
+            console.log(`Done: ${curr} : ${index + 1}`);
+            break;
+        }
+    }
+}
+
+main(path.join(__dirname, "./example.txt"));
+main(path.join(__dirname, "./input.txt"));
