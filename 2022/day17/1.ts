d4e6718d3c13b5e883e3642d43d1dad724c5b613
Stompf <anton.lundmark@gmail.com>
2022-12-31 23:18:54 +0100

    wip

--- 2022/day17/1.ts
+++ 2022/day17/1.ts
@@ -12,12 +12,14 @@ const stone3 = [
     [".", ".", "#"],
     ["#", "#", "#"],
 ];
-const stone4 = [
+
+const stone4 = [["#"], ["#"], ["#"], ["#"]];
+const stone5 = [
     ["#", "#"],
     ["#", "#"],
 ];
 
-const STONE_ROTATION = [stone1, stone2, stone3, stone4];
+const STONE_ROTATION = [stone1, stone2, stone3, stone4, stone5];
 
 interface BoundingBox {
     width: number;
@@ -26,20 +28,23 @@ interface BoundingBox {
 
 class Stone {
     public bbox: BoundingBox;
+    private body: string[][];
 
-    constructor(private body: string[][], public x: number, public y: number) {
+    constructor(body: string[][], public x: number, public y: number) {
         this.bbox = {
             width: body[0].length,
             height: body.length,
         };
+
+        this.body = body.slice();
     }
 
     public contains(other: Stone) {
         const bboxContains =
-            this.x <= other.x &&
-            this.y <= other.y &&
-            this.x + this.bbox.width - 1 >= other.x + other.bbox.width - 1 &&
-            this.y + this.bbox.height - 1 >= other.y + other.bbox.height - 1;
+            this.x <= other.x + other.bbox.width - 1 &&
+            this.x + this.bbox.width - 1 >= other.x &&
+            this.y >= other.y - other.bbox.height + 1 &&
+            this.y - this.bbox.height + 1 <= other.y;
 
         if (!bboxContains) {
             return false;
@@ -47,31 +52,37 @@ class Stone {
 
         // const minX = Math.min(this.x, other.x);
         // const maxX = Math.max(this.x + this.bbox.width - 1, other.x + other.bbox.width - 1);
-        const minY = Math.min(this.y, other.y);
-        const maxY = Math.max(this.y + this.bbox.height - 1, other.y + other.bbox.height - 1);
+        const minY = Math.min(this.y - this.bbox.height + 1, other.y - this.bbox.height + 1);
+        const maxY = Math.max(this.y, other.y);
 
-        const arr: string[][] = [];
+        let arr: string[][] = [];
 
         for (let yIndex = minY; yIndex <= maxY; yIndex++) {
             arr[yIndex] = [];
         }
 
-        for (let yIndex = 0; yIndex <= this.body.length; yIndex++) {
-            for (let xIndex = 0; xIndex <= this.body[yIndex].length; xIndex++) {
-                arr[this.y + yIndex][this.x + xIndex] = this.body[yIndex][xIndex];
+        for (let yIndex = 0; yIndex < this.body.length; yIndex++) {
+            for (let xIndex = 0; xIndex < this.body[yIndex].length; xIndex++) {
+                arr[this.y - yIndex][this.x + xIndex] = this.body[yIndex][xIndex];
             }
         }
 
-        for (let yIndex = 0; yIndex <= other.body.length; yIndex++) {
-            for (let xIndex = 0; xIndex <= other.body[yIndex].length; xIndex++) {
-                const t = this.body[yIndex][xIndex];
-                if (t === "#" && arr[this.y + yIndex][this.x + xIndex] === "#") {
+        // arr = arr.reverse();
+
+        for (let yIndex = 0; yIndex < other.body.length; yIndex++) {
+            for (let xIndex = 0; xIndex < other.body[yIndex].length; xIndex++) {
+                const t = other.body[yIndex][xIndex];
+                if (t === "#" && arr[other.y - yIndex]?.[other.x + xIndex] === "#") {
                     return true;
                 }
             }
         }
 
         return false;
+    }
+
+    public clone() {
+        return new Stone(this.body, this.x, this.y);
     }
 }
 
@@ -81,25 +92,69 @@ function main(path: string) {
 
     let currStoneIndex = 0;
     let currDirection = 0;
-    const stone = new Stone(STONE_ROTATION[currStoneIndex], 2, 4);
+    let maxY = 0;
+    let stone = new Stone(STONE_ROTATION[currStoneIndex], 2, 4);
     const stones: Stone[] = [];
 
     stones.push(stone);
-    for (let step = 1; step < 2023; step++) {
+    while (stones.length < 2023) {
+        const clone = stone.clone();
+
         const windDirection = windDirections[currDirection];
 
         switch (windDirection) {
             case "<":
-                stone.x = Math.max(0, stone.x - 1);
+                clone.x = Math.max(0, clone.x - 1);
                 break;
             case ">":
-                stone.x = Math.min(6, stone.x + 1);
+                clone.x = Math.min(clone.x + 1, 7 - clone.bbox.width);
                 break;
             default:
                 throw new Error(`Unknown direction ${windDirection}`);
         }
+
+        let collide = stones.some((s) => {
+            if (s === stone) {
+                return false;
+            }
+            return s.contains(clone);
+        });
+
+        if (collide) {
+            clone.x = stone.x;
+        }
+
+        stone.x = clone.x;
+        clone.y -= 1;
+
+        collide = stones.some((s) => {
+            if (s === stone) {
+                return false;
+            }
+            return s.contains(clone);
+        });
+
+        if (clone.y === clone.bbox.height - 1 || collide) {
+            maxY = Math.max(stone.y, maxY);
+            currStoneIndex++;
+            if (currStoneIndex > STONE_ROTATION.length - 1) {
+                currStoneIndex = 0;
+            }
+            const body = STONE_ROTATION[currStoneIndex];
+            stone = new Stone(body, 2, maxY + 3 + body.length);
+            stones.push(stone);
+        } else {
+            stone.y = clone.y;
+        }
+
+        currDirection++;
+        if (currDirection > windDirections.length - 1) {
+            currDirection = 0;
+        }
     }
+
+    console.log(maxY);
 }
 
 main(path.join(__dirname, "./example.txt"));
-// main(path.join(__dirname, "./input.txt"));
+main(path.join(__dirname, "./input.txt"));
--- 2022/day17/2.ts
+++ 2022/day17/2.ts
@@ -1,149 +1,164 @@
 import path from "path";
 import fs from "fs";
-import { trim } from "lodash";
 
-interface Valve {
-    name: string;
-    rate: number;
-    leadsTo: string[];
+const stone1 = [["#", "#", "#", "#"]];
+const stone2 = [
+    [".", "#", "."],
+    ["#", "#", "#"],
+    [".", "#", "."],
+];
+const stone3 = [
+    [".", ".", "#"],
+    [".", ".", "#"],
+    ["#", "#", "#"],
+];
+
+const stone4 = [["#"], ["#"], ["#"], ["#"]];
+const stone5 = [
+    ["#", "#"],
+    ["#", "#"],
+];
+
+const STONE_ROTATION = [stone1, stone2, stone3, stone4, stone5];
+
+interface BoundingBox {
+    width: number;
+    height: number;
 }
 
-const distanceMemo = new Map();
+class Stone {
+    public bbox: BoundingBox;
+    private body: string[][];
 
-const distanceMemoKey = (currValve: Valve, targetValve: Valve) => {
-    return currValve.name < targetValve.name
-        ? currValve.name + targetValve.name
-        : targetValve.name + currValve.name;
-};
+    constructor(body: string[][], public x: number, public y: number) {
+        this.bbox = {
+            width: body[0].length,
+            height: body.length,
+        };
 
-function main(path: string) {
-    const valves = fs
-        .readFileSync(path, "utf-8")
-        .split("\n")
-        .filter(Boolean)
-        .map((line): Valve => {
-            const str = line
-                .replace("Valve ", "")
-                .replace(" has flow rate=", ",")
-                .replace("; tunnels lead to valves ", ",")
-                .replace("; tunnel leads to valve ", ",")
-                .split(",");
-
-            return {
-                name: str[0].trim(),
-                rate: Number(str[1].trim()),
-                leadsTo: str.slice(2).map(trim),
-            };
-        });
-
-    const nextOptimalValve = (currValve: Valve, timeLeft: number, contesters: Valve[]) => {
-        let optimalValve = null;
-        let value = 0;
-
-        for (let contester of contesters) {
-            let newContesters = [...contesters].filter((v) => v.name !== contester.name);
-            let newTime = timeLeft - distanceTo(currValve, contester) - 1;
-            if (newTime <= 0) {
-                continue;
-            }
-            let score = newTime * contester.rate;
-            let optimal = nextOptimalValve(contester, newTime, newContesters);
-            score += optimal.value;
-
-            if (score > value) {
-                optimalValve = contester;
-                value = score;
-            }
-        }
-
-        return { optimalValve, value };
-    };
-
-    const distanceTo = (currValve: Valve, targetValve: Valve) => {
-        let key = distanceMemoKey(currValve, targetValve);
-        if (distanceMemo.has(key)) {
-            return distanceMemo.get(key);
-        }
-        let visited = new Set<string>();
-        let queue = [currValve];
-        let traveled = 0;
-
-        while (queue.length > 0) {
-            let nextQueue = [];
-            for (let valve of queue) {
-                if (visited.has(valve.name)) {
-                    continue;
-                }
-                visited.add(valve.name);
-                if (valve.name === targetValve.name) {
-                    distanceMemo.set(key, traveled);
-                    return traveled;
-                }
-                for (let neighbor of valve.leadsTo) {
-                    const n = valves.find((v) => v.name === neighbor);
-                    nextQueue.push(n!);
-                }
-            }
-            queue = nextQueue;
-            traveled++;
-        }
-    };
-
-    const calcScore = (myValves: Valve[], elephantValves: Valve[]) => {
-        let score = 0;
-        for (let i = 0; i < myValves.length; i++) {
-            for (let j = i + 1; j < myValves.length; j++) {
-                score += distanceTo(myValves[i], myValves[j]);
-            }
-        }
-        for (let i = 0; i < elephantValves.length; i++) {
-            for (let j = i + 1; j < elephantValves.length; j++) {
-                score += distanceTo(elephantValves[i], elephantValves[j]);
-            }
-        }
-
-        return score;
-    };
-
-    const findBestSplits = (contesters: Valve[]) => {
-        let score = Infinity;
-        let myValves: Valve[] = [];
-        let elephantValves: Valve[] = [];
-
-        // Random paths
-        for (let i = 0; i < 100000; i++) {
-            let elephantValvesTest = [];
-            let myValvesTest = [];
-            for (let contester of contesters) {
-                if (Math.random() > 0.5) {
-                    myValvesTest.push(contester);
-                } else {
-                    elephantValvesTest.push(contester);
-                }
-            }
-            let newScore = calcScore(myValvesTest, elephantValvesTest);
-            if (newScore < score) {
-                score = newScore;
-                myValves = myValvesTest;
-                elephantValves = elephantValvesTest;
-            }
-        }
-        return { myValves, elephantValves };
-    };
-
-    const start = valves.find(({ name }) => name === "AA");
-    if (!start) {
-        throw new Error("Could not find valve AA");
+        this.body = body.slice();
     }
 
-    const contesters = valves.filter(({ rate }) => rate > 0);
-    const { myValves, elephantValves } = findBestSplits(contesters);
+    public contains(other: Stone) {
+        const bboxContains =
+            this.x <= other.x + other.bbox.width - 1 &&
+            this.x + this.bbox.width - 1 >= other.x &&
+            this.y >= other.y - other.bbox.height + 1 &&
+            this.y - this.bbox.height + 1 <= other.y;
 
-    const my = nextOptimalValve(start, 26, myValves).value;
-    const eleph = nextOptimalValve(start, 26, elephantValves).value;
+        if (!bboxContains) {
+            return false;
+        }
 
-    console.log(my + eleph);
+        // const minX = Math.min(this.x, other.x);
+        // const maxX = Math.max(this.x + this.bbox.width - 1, other.x + other.bbox.width - 1);
+        const minY = Math.min(this.y - this.bbox.height + 1, other.y - this.bbox.height + 1);
+        const maxY = Math.max(this.y, other.y);
+
+        let arr: string[][] = [];
+
+        for (let yIndex = minY; yIndex <= maxY; yIndex++) {
+            arr[yIndex] = [];
+        }
+
+        for (let yIndex = 0; yIndex < this.body.length; yIndex++) {
+            for (let xIndex = 0; xIndex < this.body[yIndex].length; xIndex++) {
+                arr[this.y - yIndex][this.x + xIndex] = this.body[yIndex][xIndex];
+            }
+        }
+
+        // arr = arr.reverse();
+
+        for (let yIndex = 0; yIndex < other.body.length; yIndex++) {
+            for (let xIndex = 0; xIndex < other.body[yIndex].length; xIndex++) {
+                const t = other.body[yIndex][xIndex];
+                if (t === "#" && arr[other.y - yIndex]?.[other.x + xIndex] === "#") {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public clone() {
+        return new Stone(this.body, this.x, this.y);
+    }
 }
 
-main(path.join(__dirname, "./example.txt"));
+function main(path: string) {
+    const windDirections = fs.readFileSync(path, "utf-8").split("").filter(Boolean);
+    console.log(windDirections);
+
+    let currStoneIndex = 0;
+    let currDirection = 0;
+    let maxY = 0;
+    let stone = new Stone(STONE_ROTATION[currStoneIndex], 2, 4);
+    const stones: Stone[] = [];
+
+    stones.push(stone);
+    while (stones.length < 1000000000001) {
+        const clone = stone.clone();
+
+        const windDirection = windDirections[currDirection];
+
+        switch (windDirection) {
+            case "<":
+                clone.x = Math.max(0, clone.x - 1);
+                break;
+            case ">":
+                clone.x = Math.min(clone.x + 1, 7 - clone.bbox.width);
+                break;
+            default:
+                throw new Error(`Unknown direction ${windDirection}`);
+        }
+
+        let collide = stones.some((s) => {
+            if (s === stone) {
+                return false;
+            }
+            return s.contains(clone);
+        });
+
+        if (collide) {
+            clone.x = stone.x;
+        }
+
+        stone.x = clone.x;
+        clone.y -= 1;
+
+        collide = stones.some((s) => {
+            if (s === stone) {
+                return false;
+            }
+            return s.contains(clone);
+        });
+
+        if (clone.y === clone.bbox.height - 1 || collide) {
+            maxY = Math.max(stone.y, maxY);
+            currStoneIndex++;
+            if (currStoneIndex > STONE_ROTATION.length - 1) {
+                currStoneIndex = 0;
+            }
+            const body = STONE_ROTATION[currStoneIndex];
+            stone = new Stone(body, 2, maxY + 3 + body.length);
+            stones.push(stone);
+
+            if (stones.length % 10000 === 0) {
+                console.log(stones.length);
+            }
+        } else {
+            stone.y = clone.y;
+        }
+
+        currDirection++;
+        if (currDirection > windDirections.length - 1) {
+            currDirection = 0;
+        }
+    }
+
+    console.log(maxY);
+}
+
+// main(path.join(__dirname, "./example.txt"));
 main(path.join(__dirname, "./input.txt"));
